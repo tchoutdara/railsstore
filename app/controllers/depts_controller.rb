8ffda753abb4e4c9b066e21880588001c7a1cf36
tchoutdara <monkeymilitia444@gmail.com>
2018-09-04 22:42:59 -0600

    fix typos

--- app/controllers/depts_controller.rb
+++ app/controllers/depts_controller.rb
@@ -17,7 +17,7 @@ class DeptsController < ApplicationController
     @dept = Dept.new(dept_params)
 
     if @dept.save
-      redirect_to dept_path
+      redirect_to depts_path
     else
       render :new
     end
@@ -37,7 +37,7 @@ class DeptsController < ApplicationController
 
   def destroy
     @dept.destroy
-    redirect_to dept_path
+    redirect_to depts_path
   end
 
   private
